ead2b39eccfcc95ce97c072da5147e9948e63531
fran-tn <francisconun8mail.com>
2025-03-31 22:23:45 -0300

    agregar puntero

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 int main()
-{
-    printf("Hola mundo");
+{   
+    
+    printf("\n Hola mundo");
+
+    int numero = 7, *puntero;
+        puntero = &numero;
+        
+    printf("EL contenido del puntero es: %p", puntero);
+    printf("\n La direccion de memoria almacenada por el puntero es: %p", puntero);
+    printf("\n La direccion de memoria de la variable es: %p", &numero);
+    printf("\n La direccion del puntero es: %p", &puntero);
+    printf("\n El tamaño de memoria de la variable es: %zu bytes", sizeof(numero));
+
+   
     return 0;
 }
